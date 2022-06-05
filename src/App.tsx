3fe44a847e5342303eb552172a0c0ef1d463dbe2
mmerezhko <mmere@softserveinc.com>
2022-06-05 18:28:03 +0300

    Import new icons for tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Component, lazy } from 'solid-js';
+import { Component, lazy, Suspense } from 'solid-js';
 import { Routes, Route, Link } from 'solid-app-router';
 
 import SuperEllipse from './components/Superellipse';
+import Loader from './components/Loader';
 
 import BagIcon from './assets/icons/bag.svg';
+import SquareTextIcon from './assets/icons/square-text.svg';
+import PersonFinderIcon from './assets/icons/person-finder.svg';
 
 const Home = lazy(() => import('./pages/Home'));
 const NewsFeed = lazy(() => import('./pages/NewsFeed'));
@@ -12,27 +15,37 @@ const App: Component = () => {
   return (
     <div class='flex col items-center'>
       <main class='app content-full'>
-        <Routes>
-          <Route path='/' element={<Home />} />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/' element={<Home />} />
 
-          <Route path='/news' element={<NewsFeed />} />
-        </Routes>
+            <Route path='/news' element={<NewsFeed />} />
+          </Routes>
+        </Suspense>
       </main>
       <nav
         class='fixed-bottom dock rounded content-full flex justify-between'
         aria-label='Tabs'
       >
-        <Link href='/'>
+        <Link href='/' aria-label='Home'>
           <SuperEllipse>
             <BagIcon />
           </SuperEllipse>
         </Link>
 
-        <SuperEllipse>Store</SuperEllipse>
+        <Link href='/news' aria-label='News'>
+          <SuperEllipse>
+            <SquareTextIcon />
+          </SuperEllipse>
+        </Link>
 
-        <SuperEllipse>Settings</SuperEllipse>
+        <SuperEllipse>Soon</SuperEllipse>
 
-        <SuperEllipse>Messages</SuperEllipse>
+        <Link href='/profile' aria-label='News'>
+          <SuperEllipse>
+            <PersonFinderIcon />
+          </SuperEllipse>
+        </Link>
       </nav>
     </div>
   );
