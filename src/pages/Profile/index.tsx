6aafe6af9e15de66ed3a8a031bff01a5e67a04ce
mmerezhko <mmere@softserveinc.com>
2022-06-12 16:27:07 +0300

    Working on theme design

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import type { Component } from 'solid-js';
+import { createSignal, Component } from 'solid-js';
 
 import Header from '../../components/Header';
 
@@ -6,65 +6,80 @@ import FaceDashedIcon from '../../assets/icons/face-dashed.svg';
 
 import './Profile.css';
 
-const Profile: Component = () => (
-  <div class='layer view rounded content-full'>
-    <header class='flex justify-between items-center'>
-      <strong class='subtitle'>Your Profile</strong>
+const Profile: Component = () => {
+  const [theme, setTheme] = createSignal<string>('');
 
-      <FaceDashedIcon />
-    </header>
+  const handleChange = (e: any) => setTheme(e.target.value);
 
-    <Header spot='Your new superpower' />
+  const handleSubmit = () => {
+    document.body.className = theme();
+  };
 
-    <b class='badge flex items-center rounded info' id='edge-design'>
-      <i class='indicator'></i>Design Gallery
-    </b>
+  return (
+    <div class='layer view rounded content-full'>
+      <header class='flex justify-between items-center'>
+        <strong class='subtitle'>Your Profile</strong>
 
-    <ul
-      role='radiogroup'
-      class='flex col theme-list'
-      aria-labelledby='edge-design'
-    >
-      <li class='flex items-center'>
-        <input
-          type='radio'
-          id='green-theme'
-          name='app-theme'
-          value='Green'
-          class='form-radio'
-        />
-        <label for='green-theme' class='form-label'>
-          Green
-        </label>
-      </li>
-      <li class='flex items-center'>
-        <input
-          type='radio'
-          id='blue-theme'
-          name='app-theme'
-          value='Blue'
-          class='form-radio'
-        />
-        <label for='blue-theme' class='form-label'>
-          Blue
-        </label>
-      </li>
-      <li class='flex items-center'>
-        <input
-          type='radio'
-          id='night-theme'
-          name='app-theme'
-          value='Midnight'
-          class='form-radio'
-        />
-        <label for='night-theme' class='form-label'>
-          Midnight
-        </label>
-      </li>
-    </ul>
+        <FaceDashedIcon />
+      </header>
 
-    <button class='btn'>Update theme</button>
-  </div>
-);
+      <Header spot='Your new superpower' />
+
+      <b class='badge flex items-center rounded info' id='edge-design'>
+        <i class='indicator'></i>Design Gallery
+      </b>
+
+      <ul
+        role='radiogroup'
+        class='flex col theme-list rounded'
+        aria-labelledby='edge-design'
+      >
+        <li class='flex items-center'>
+          <input
+            type='radio'
+            id='green-theme'
+            name='app-theme'
+            value='green-theme'
+            class='form-radio'
+            onChange={handleChange}
+          />
+          <label for='green-theme' class='form-label'>
+            Green
+          </label>
+        </li>
+        <li class='flex items-center'>
+          <input
+            type='radio'
+            id='blue-theme'
+            name='app-theme'
+            value='blue-theme'
+            class='form-radio'
+            onChange={handleChange}
+          />
+          <label for='blue-theme' class='form-label'>
+            Blue
+          </label>
+        </li>
+        <li class='flex items-center'>
+          <input
+            type='radio'
+            id='night-theme'
+            name='app-theme'
+            value='night-theme'
+            class='form-radio'
+            onChange={handleChange}
+          />
+          <label for='night-theme' class='form-label'>
+            Midnight
+          </label>
+        </li>
+      </ul>
+
+      <button class='btn' onClick={handleSubmit}>
+        Update theme
+      </button>
+    </div>
+  );
+};
 
 export default Profile;
