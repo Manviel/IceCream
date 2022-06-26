5ac33bc4a5ffbffe85192dfb4be08d2e6f1fabc7
mmerezhko <mmere@softserveinc.com>
2022-06-26 15:14:53 +0300

    Create button to refetch resource

--- src/components/Card/Quote.tsx
+++ src/components/Card/Quote.tsx
@@ -4,12 +4,14 @@ import Loader from '../Loader';
 
 import { getQuote } from '../../services/news';
 
+import GoForwardIcon from '../../assets/icons/go-forward.svg';
+
 import './DateBox.css';
 
 const fetchQuote = async () => await getQuote();
 
 const Quote: Component = () => {
-  const [quote] = createResource(fetchQuote);
+  const [quote, { refetch }] = createResource(fetchQuote);
 
   return (
     <Switch fallback={<h2 class='box view rounded info'>Failed to fetch</h2>}>
@@ -18,14 +20,24 @@ const Quote: Component = () => {
       </Match>
       <Match when={quote()}>
         {(res) => (
-          <div class='box view rounded info'>
-            <p class='flex justify-between'>
-              <span>{res.author}</span>
-              <time>{res.dateModified}</time>
-            </p>
+          <article class='box view rounded info'>
+            <div class='flex justify-between items-center'>
+              <p class='flex col'>
+                <time>{res.dateModified}</time>
+                <span>{res.author}</span>
+              </p>
+
+              <button
+                class='superellipse'
+                onClick={refetch}
+                aria-label='Get new quote'
+              >
+                <GoForwardIcon />
+              </button>
+            </div>
 
             <h2 class='info'>{res.content}</h2>
-          </div>
+          </article>
         )}
       </Match>
     </Switch>
