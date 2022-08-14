d63e408bed541c6049bf3bf4dd65974f3bd50df4
mmerezhko <mmere@softserveinc.com>
2022-08-14 12:00:49 +0300

    Imrove skeleton layout

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,9 +4,9 @@ import './Loader.css';
 
 const Loader: Component = () => (
   <aside class='pulse-loading view rounded content-full' aria-busy>
-    <div class='loader rounded'></div>
-    <div class='loader rounded'></div>
-    <div class='loader rounded'></div>
+    <p class='loader box rounded'>Loading...</p>
+    <div class='loader box rounded'></div>
+    <div class='spinner box rounded'></div>
   </aside>
 );
 
@@ -14,15 +14,15 @@ export const Skeleton: Component = () => (
   <>
     <aside class='pulse-loading view rounded content-full' aria-busy>
       <header class='flex justify-between items-center'>
-        <div class='subtitle skeleton loader rounded'></div>
-        <div class='skeleton loader rounded '></div>
+        <div class='subtitle skeleton loader box rounded'></div>
+        <div class='skeleton loader box rounded'></div>
       </header>
-      <div class='title loader rounded'></div>
-      <div class='info loader rounded'></div>
-      <div class='screen loader rounded'></div>
+      <div class='title loader box rounded'></div>
+      <p class='info loader box rounded'>Please wait...</p>
+      <div class='screen loader box rounded'></div>
       <footer class='products'>
-        <div class='loader rounded content-full'></div>
-        <div class='loader rounded content-full'></div>
+        <div class='loader box rounded content-full'></div>
+        <div class='loader box rounded content-full'></div>
       </footer>
     </aside>
 
