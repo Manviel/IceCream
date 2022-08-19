dc13507400dcdb6dc11c68478ef4792affd30371
mmerezhko <mmere@softserveinc.com>
2022-08-19 16:45:27 +0300

    Import player icons

--- src/pages/Profile/Expert/index.tsx
+++ src/pages/Profile/Expert/index.tsx
@@ -3,6 +3,12 @@ import { Tab, TabGroup, TabPanel, Tabs } from 'solid-a11y';
 
 import Card from '../../../components/Card';
 import PageDecorator from '../../../components/PageDecorator';
+import { SuperEllipse } from '../../../components/Superellipse';
+import ConnectFactory from '../../../components/ConnectFactory';
+
+import PlayNextIcon from '../../../assets/icons/play-next.svg';
+import PlayPrevIcon from '../../../assets/icons/play-prev.svg';
+import PlayFillIcon from '../../../assets/icons/play-fill.svg';
 
 import '../../Home/Home.css';
 import './Expert.css';
@@ -65,16 +71,51 @@ const Expert: Component = () => {
                     description='Coming Soon'
                   />
 
-                  <div class='view'>
-                    <button>Previous</button>
-                    <button>Play</button>
-                    <button>Next</button>
+                  <div class='flex col view'>
+                    <nav class='flex justify-between'>
+                      <button class={SuperEllipse} aria-label='Prev'>
+                        <PlayPrevIcon />
+                      </button>
+                      <button class={SuperEllipse} aria-label='Auto Play'>
+                        <PlayFillIcon />
+                      </button>
+                      <button class={SuperEllipse} aria-label='Next'>
+                        <PlayNextIcon />
+                      </button>
+                    </nav>
+
+                    <div class='flex justify-between'>
+                      <article class='flex col text-center'>
+                        <h2 class='info'>Ratings</h2>
+                        <p>4 out of 5</p>
+                      </article>
+
+                      <article class='flex col text-center'>
+                        <h2 class='info'>Usage</h2>
+                        <p>900 million</p>
+                      </article>
+
+                      <article class='flex col text-center'>
+                        <h2 class='info'>Report</h2>
+
+                        <ConnectFactory href='https://www.digitalocean.com/community/tutorials/gangs-of-four-gof-design-patterns' />
+                      </article>
+                    </div>
                   </div>
                 </div>
 
-                <h2 class='subtitle'>Favourites</h2>
+                <h3 class='subtitle'>Favourites</h3>
 
                 <p class='info'>{item.explanation}</p>
+
+                <article class='flex col'>
+                  <h4 class='subtitle'>Reviews</h4>
+                  <p class='info'>
+                    Once the business analysis has communicated with
+                    stakeholders for understanding their requirements, it can be
+                    described as elicitation.
+                  </p>
+                </article>
               </TabPanel>
             )}
           </For>
