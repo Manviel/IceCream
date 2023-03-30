9ef2e48049c06dcc395f7f8ca82a4e0f3ac7b91e
mmerezhko <mmere@softserveinc.com>
2023-03-30 17:54:02 +0300

    Create more unique colors for slides

--- src/pages/Profile/Expert/Reviews.tsx
+++ src/pages/Profile/Expert/Reviews.tsx
@@ -96,7 +96,7 @@ const Reviews: Component = () => {
           their thoughts. After collecting the responses from stakeholders, data
           is analyzed to identify the area of interest of stakeholders.
         </p>
-        <p class='info price view rounded'>
+        <p class='info layer view rounded'>
           Prototyping is used to identify missing or unspecified requirements.
           In this technique, frequent demos are given to the client by creating
           the prototypes so that client can get an idea of how the product will
