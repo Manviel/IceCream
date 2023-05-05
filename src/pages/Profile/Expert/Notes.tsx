1ff3fb53b0e11d07990f653f874279b37378cecb
mmerezhko <mmere@softserveinc.com>
2023-05-05 17:54:25 +0300

    Add link about Business metrics

--- src/pages/Profile/Expert/Notes.tsx
+++ src/pages/Profile/Expert/Notes.tsx
@@ -3,6 +3,7 @@ import { Component } from 'solid-js';
 import { ActionTypes } from '../../../models/config';
 
 import DialogFacade from '../../../components/DialogContent/DialogFacade';
+import ConnectFactory from '../../../components/ConnectFactory';
 
 const SheetContent = `The Event Loop has one simple job — to monitor the Call Stack and the
 Callback Queue. If the Call Stack is empty, the Event Loop will take the
@@ -26,7 +27,22 @@ const Notes: Component = () => {
       parentClassName='bottom-sheet view card'
       childClassName='flex col'
       toggleActionSheet={toggleActionSheet}
-    />
+    >
+      <div class='flex col gap'>
+        <ConnectFactory
+          href='https://finviz.com/'
+          text='Finviz Stock screener'
+        />
+        <ConnectFactory
+          href='https://www.w3.org/WAI/business-case/'
+          text='Business case for Accessibility'
+        />
+        <ConnectFactory
+          href='https://www.appcues.com/blog/saas-growth-metrics'
+          text='Business metrics that matter'
+        />
+      </div>
+    </DialogFacade>
   );
 };
 
