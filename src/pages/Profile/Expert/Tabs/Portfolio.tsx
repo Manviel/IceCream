d5be290591740064f755271635561fd8105c3050
mmerezhko <mmere@softserveinc.com>
2023-08-16 17:46:41 +0300

    Update lockup to reduce noise

--- src/pages/Profile/Expert/Tabs/Portfolio.tsx
+++ src/pages/Profile/Expert/Tabs/Portfolio.tsx
@@ -4,8 +4,9 @@ import DocCopyIcon from '../../../../assets/icons/doc-copy.svg';
 
 import { IDType } from '../../../../models';
 
-import Notes from '../Notes';
 import Tooltip from '../../../../components/Tooltip';
+
+import Notes from '../Notes';
 
 interface PortfolioType<T> extends IDType {
   fairPriceCost: Accessor<T>;
@@ -20,9 +21,7 @@ const Portfolio: ParentComponent<PortfolioType<number>> = (props) => {
 
   const [snackbar, setSnackbar] = createSignal<string>(INIT_HELP);
 
-  const handleResetTooltip = () => {
-    setSnackbar(INIT_HELP);
-  };
+  const handleResetTooltip = () => setSnackbar(INIT_HELP);
 
   const handleCopy = async () => {
     try {
@@ -40,32 +39,30 @@ const Portfolio: ParentComponent<PortfolioType<number>> = (props) => {
 
       {children}
 
-      <div class='flex col ghost os' role='status'>
-        <p>Fair Price (in $)</p>
-
-        <div class='flex items-center justify-between'>
+      <div class='flex items-center justify-between items-end ghost os'>
+        <div class='flex col lockup' role='status'>
+          <p>Fair Price (in $)</p>
           <strong class='subtitle'>{fairPriceCost().toFixed(2)}</strong>
-
-          <Tooltip
-            id={ID_HELP}
-            name='Clone'
-            onClick={handleCopy}
-            onClose={handleResetTooltip}
-            snackbar={snackbar}
-          >
-            <DocCopyIcon />
-          </Tooltip>
         </div>
+
+        <Tooltip
+          id={ID_HELP}
+          name='Clone'
+          onClick={handleCopy}
+          onClose={handleResetTooltip}
+          snackbar={snackbar}
+        >
+          <DocCopyIcon />
+        </Tooltip>
       </div>
 
-      <div class='flex col document os' role='log'>
-        <p>Fair Price (in %)</p>
-
-        <div class='flex items-center justify-between'>
+      <div class='flex items-center justify-between items-end document os'>
+        <div class='flex col lockup' role='status'>
+          <p>Fair Price (in %)</p>
           <strong class='subtitle'>{fairPricePercent(fairPriceCost())}</strong>
-
-          <Notes />
         </div>
+
+        <Notes />
       </div>
     </fieldset>
   );
