69a3480d8b7b50965ea61a74f89d193e2addef21
Mike <mmieriezhko@outlook.com>
2024-06-23 15:07:22 +0000

    Import listbox component

--- src/pages/Bag/Strategy.tsx
+++ src/pages/Bag/Strategy.tsx
@@ -1,5 +1,8 @@
 import { For, Component } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
+import { Select } from '@kobalte/core/select';
+
+import ArrowDownIcon from '../../assets/icons/arrow-down-circle.svg';
 
 export type OptionType = {
   name: string;
@@ -36,15 +39,36 @@ export const TextOption = (name: string, values: string[]): OptionType => ({
   name,
   values,
   render: (props) => {
-    const execute = ({ currentTarget }: any) =>
-      props.onChange(name, currentTarget.value);
+    const execute = (v: string) => props.onChange(name, v);
 
     return (
-      <select onChange={execute}>
-        <For each={values}>
-          {(value) => <option value={value}>{value}</option>}
-        </For>
-      </select>
+      <Select
+        onChange={execute}
+        options={values}
+        class="provision flex col"
+        itemComponent={(props) => (
+          <Select.Item item={props.item} class="select_item control">
+            <Select.ItemLabel>{props.item.rawValue}</Select.ItemLabel>
+          </Select.Item>
+        )}
+      >
+        <Select.Label class="select_label">{name}</Select.Label>
+
+        <Select.Trigger class="select_trigger flex justify-between items-center token control">
+          <Select.Value<string> class="concise">
+            {(state) => state.selectedOption()}
+          </Select.Value>
+          <Select.Icon class="select_icon flex">
+            <ArrowDownIcon />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content class="select_content control">
+            <Select.Listbox class="select_listbox flex col tab" />
+          </Select.Content>
+        </Select.Portal>
+      </Select>
     );
   },
 });
@@ -61,15 +85,10 @@ export const OptionSelector: Component<OptionSelectorProps> = (props) => {
 
   return (
     <fieldset>
-      <legend class='card-sub'>Customize Your Laptop</legend>
+      <legend class="card-sub">Customize Your Laptop</legend>
 
       <For each={options}>
-        {(optionType) => (
-          <div class='provision'>
-            <label>{optionType.name}: </label>
-            {optionType.render({ onChange: handleChange })}
-          </div>
-        )}
+        {(optionType) => optionType.render({ onChange: handleChange })}
       </For>
     </fieldset>
   );
