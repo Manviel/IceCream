3ed77d2c18063628c87000a255150e16372d19e7
Manviel <mmere@softserveinc.com>
2025-12-10 13:25:12 +0200

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import solid from 'eslint-plugin-solid';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
   { languageOptions: { globals: globals.browser } },
   {
     ignores: ['**/node_modules/*', 'dist/*']
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: { solid },
+    rules: solid.configs.typescript.rules
+  }
 ];
